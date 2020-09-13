150a3b35bf253b2a1770d3a260e872a4749dc130
krodetsa <krodetsa@gmail.com>
2020-09-13 11:17:14 +0300

    gitignore fix

---
+++ README.md
@@ -0,0 +1,2 @@
+# react-ionic-beacon-checker
+Simple iBeacon checker powered by React Js and Ionic framework
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,9 +1,60 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import React, { useState }  from 'react';
+import { IonContent,IonLoading, IonButton, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './Tab1.css';
-
+import { IBeacon } from '@ionic-native/ibeacon';
 const Tab1: React.FC = () => {
+  const [text, setText] = useState('Нет событий');
+  let delegate = IBeacon.Delegate();
+  delegate.didRangeBeaconsInRegion()
+  .subscribe(
+    data => console.log('didRangeBeaconsInRegion: ', data),
+    error => console.error()
+  );
+  delegate.didEnterRegion()
+  .subscribe(
+    data => {
+      setText(data.region.identifier);
+    }
+  );
+  delegate.didStartMonitoringForRegion()
+  .subscribe(
+    data => console.log('didStartMonitoringForRegion: ', data),
+    error => console.error()
+  );
+
+  let beaconsArray = [
+    {
+      name: "wEci",
+      uuid: 'f7826da6-4fa2-4e98-8024-bc5b71e0893e'
+    },
+    {
+      name: "wEc2",
+      uuid: '6665542b-41a1-5e00-931c-6a82db9b78c1'
+    },
+  ]
+  const stopScanner = () => {
+    beaconsArray.forEach(el => {
+      let beaconRegion = IBeacon.BeaconRegion(el.name, el.uuid);
+      IBeacon.stopMonitoringForRegion(beaconRegion)
+      .then(
+        () => setText('Нет событий'),
+        error => alert(error)
+      )
+    })
+
+  }
+  const openScanner = () => {
+    beaconsArray.forEach(el => {
+      let beaconRegion = IBeacon.BeaconRegion(el.name, el.uuid);
+      IBeacon.startMonitoringForRegion(beaconRegion)
+      .then(
+        () => setText('Поиск'),
+        error => alert(error)
+      );
+    })
+
+
+ };
   return (
     <IonPage>
       <IonHeader>
@@ -17,7 +68,9 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <IonButton onClick={openScanner}>Scan</IonButton>
+        <IonButton onClick={stopScanner}>Stop</IonButton>
+        <IonTitle>{text}</IonTitle>
       </IonContent>
     </IonPage>
   );
