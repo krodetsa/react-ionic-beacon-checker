90968e8faeede584af13b85b87e20ea753ff4382
krodetsa <krodetsa@gmail.com>
2020-09-15 13:50:32 +0300

    add didRangeBeaconsInRegion to check beacon range

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -4,18 +4,28 @@ import './Tab1.css';
 import { IBeacon } from '@ionic-native/ibeacon';
 const Tab1: React.FC = () => {
   const [text, setText] = useState('Нет событий');
+  const [text2, setText2] = useState('Нет событий2');
+  const [text3, setText3] = useState('Нет событий3');
+  IBeacon.requestAlwaysAuthorization();
   let delegate = IBeacon.Delegate();
   delegate.didRangeBeaconsInRegion()
   .subscribe(
-    data => console.log('didRangeBeaconsInRegion: ', data),
+    data =>  {setText2(JSON.stringify(data))},
     error => console.error()
   );
   delegate.didEnterRegion()
   .subscribe(
     data => {
-      setText(data.region.identifier);
+      setText(JSON.stringify(data));
     }
   );
+  delegate.didDetermineStateForRegion()
+  .subscribe(
+    data => {
+      setText3(JSON.stringify(data));
+    }
+  );
+
   delegate.didStartMonitoringForRegion()
   .subscribe(
     data => console.log('didStartMonitoringForRegion: ', data),
@@ -25,11 +35,15 @@ const Tab1: React.FC = () => {
   let beaconsArray = [
     {
       name: "wEci",
-      uuid: 'f7826da6-4fa2-4e98-8024-bc5b71e0893e'
+      uuid: 'f7826da6-4fa2-4e98-8024-bc5b71e0893e',
+      minor: 1000,
+      major: 5
     },
     {
       name: "wEc2",
-      uuid: '6665542b-41a1-5e00-931c-6a82db9b78c1'
+      uuid: '6665542b-41a1-5e00-931c-6a82db9b78c1',
+      minor: 1000,
+      major: 5
     },
   ]
   const stopScanner = () => {
@@ -37,11 +51,23 @@ const Tab1: React.FC = () => {
       let beaconRegion = IBeacon.BeaconRegion(el.name, el.uuid);
       IBeacon.stopMonitoringForRegion(beaconRegion)
       .then(
-        () => setText('Нет событий'),
+        () => {
+          // setText('Нет событий');
+          // setText2('Нет событий2');
+          // setText3('Нет событий3');
+      },
         error => alert(error)
-      )
+      );
+      IBeacon.stopRangingBeaconsInRegion(beaconRegion)
+      .then(
+        () => {
+          // setText('Нет событий');
+          // setText2('Нет событий2');
+          // setText3('Нет событий3');
+      },
+        error => alert(error)
+      );
     })
-
   }
   const openScanner = () => {
     beaconsArray.forEach(el => {
@@ -49,6 +75,11 @@ const Tab1: React.FC = () => {
       IBeacon.startMonitoringForRegion(beaconRegion)
       .then(
         () => setText('Поиск'),
+        error => alert(error)
+      );
+      IBeacon.startRangingBeaconsInRegion(beaconRegion)
+      .then(
+        () => console.log('startRangingBeaconsInRegion'),
         error => alert(error)
       );
     })
@@ -70,7 +101,12 @@ const Tab1: React.FC = () => {
         </IonHeader>
         <IonButton onClick={openScanner}>Scan</IonButton>
         <IonButton onClick={stopScanner}>Stop</IonButton>
-        <IonTitle>{text}</IonTitle>
+        <p>didEnterRegion</p>
+        <p>{text}</p>
+        <p>didRangeBeaconsInRegion</p>
+        <p>{text2}</p>
+        <p>didDetermineStateForRegion</p>
+        <p>{text3}</p>
       </IonContent>
     </IonPage>
   );
